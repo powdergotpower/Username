8f554ff583da55a74bfedae445406c34be1f8b15
powdergotpower <anshjotsinghsidhu@gmail.com>
2025-08-30 15:07:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from modules.all import client  # import the client from modules/all.py
+from bot_client import client
+import modules.start  # attach the /start PM handler
+# You can import group command modules later here, e.g., modules.all
 
-# Start the bot
-client.start()
 print("Bot is running...")
 client.run_until_disconnected()
